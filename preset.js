df1dc0efe2f1ecf9f1b7932e45e3ad4e781cc295
Shaun Lloyd <shaun@shauns-mbp.lan>
2022-06-28 00:23:16 -0400

    Use new babel plugin in preset

--- preset.js
+++ preset.js
@@ -1,3 +1,24 @@
+function babelDefault(config, { breadcrumbs }) {
+  if (!breadcrumbs.repositoryRoot) {
+    throw new Error('[STORYBOOK BREADBCRUMBS]: A project root is required')
+  }
+
+  if (!breadcrumbs.githubUrl) {
+    throw new Error('[STORYBOOK BREADBCRUMBS]: Your github url is required')
+  }
+
+  return {
+    ...config,
+      overrides: [{ 
+        include: /\.stories\.(j|t)sx?$/, 
+        plugins: [
+          ...config.plugins,
+          [require.resolve('./breadcrumb-plugin.js'), { projectRoot: breadcrumbs.repositoryRoot, githubUrl: breadcrumbs.githubUrl, defaultBranch: breadcrumbs.defaultBranch || 'main' }],
+        ]
+    }]
+  };
+}
+
 function config(entry = []) {
   return [...entry, require.resolve("./dist/esm/preset/preview")];
 }
@@ -9,4 +30,5 @@ function managerEntries(entry = []) {
 module.exports = {
   managerEntries,
   config,
+  babelDefault,
 };

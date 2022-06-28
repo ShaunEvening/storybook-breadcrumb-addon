bce82a4e827b1fdceff658d036668c7587296bae
Shaun Lloyd <shaun@shauns-mbp.lan>
2022-06-28 00:26:59 -0400

    Update demo storybook to use addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,20 @@
+const path = require('path');
+
 module.exports = {
   stories: [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["../preset.js", "@storybook/addon-essentials"],
+  addons: [
+    {
+      name: "../preset.js",
+      options: {
+        breadcrumbs: {
+          repositoryRoot: path.resolve(__dirname, "../"),
+          defaultBranch: 'main',
+          githubUrl: 'https://github.com/ShaunLloyd/breadcrumb-addon',
+        },
+      },
+    },
+    "@storybook/addon-essentials"],
 };
